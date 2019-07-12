cbcaf2c3a94daee8574a4f3a2662d6156e95baf3
Jimena Baripatti <jimenabaripatti@gmail.com>
2019-07-11 20:54:29 -0400

    create scrape foundations

--- scrape_foundations.py
+++ scrape_foundations.py
@@ -1,9 +1,8 @@
 
 import pause
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
 from bs4 import BeautifulSoup
+import pprint as pp
 
 
 options = webdriver.ChromeOptions()
@@ -22,7 +21,26 @@ pause.seconds(2)
 page_source = driver.page_source
 
 content = BeautifulSoup(page_source, 'html.parser')
-print(content)
+
+#all_text = content.get_text()
+
+
+# get all brands on the page, it results in a <class 'bs4.element.ResultSet'> iterable
+all_brands = content.find_all(attrs= {"data-at" : "sku_item_brand"})
+
+
+# print(all_brands.prettify())
+print(type(all_brands))
+
+brands = []
+for brand in all_brands:
+    brand_name = brand.get_text()
+    print(brand_name)
+    brands.append(brand_name)
+
+
+
+
 
 
 driver.close()
